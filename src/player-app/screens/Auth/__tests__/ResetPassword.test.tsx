b000c7f2cf8bed8f082b665a6226beb6c18ec062
Kristijan Karanfiloski <kikaccc29@gmail.com>
2023-12-20 12:24:52 +0100

    'added more test to the reset password test '

--- src/player-app/screens/Auth/__tests__/ResetPassword.test.tsx
+++ src/player-app/screens/Auth/__tests__/ResetPassword.test.tsx
@@ -1,24 +1,11 @@
-import {
-  fireEvent,
-  render,
-  screen,
-  waitFor
-} from '@testing-library/react-native';
+import { fireEvent, render, screen } from '@testing-library/react-native';
 import ResetPassword from '../ResetPassword';
-import auth from '@react-native-firebase/auth';
 
 jest.mock('@react-native-firebase/auth', () => {
-  return {
-    __esModule: true,
-    default: () => ({
-      sendPasswordResetEmail: jest.fn(() => Promise.resolve())
-      // Add other methods if needed
-    })
-  };
+  return () => ({
+    sendPasswordResetEmail: jest.fn().mockResolvedValue(undefined)
+  });
 });
-// const sendPasswordResetEmail = jest.fn(() => {
-//   return Promise.resolve('test@test.com');
-// });
 
 describe('ResetPassword', () => {
   it('component should render without crashing', () => {
@@ -60,7 +47,44 @@ describe('ResetPassword', () => {
     expect(sendResetPasswordButton).not.toBeDisabled();
   });
 
+  it('after pressing the Go Back button to take me back to the login page', () => {
+    const mockGoBack = jest.fn();
+    const mockNavigation = {
+      goBack: mockGoBack
+    };
+
+    render(<ResetPassword navigation={mockNavigation} route={() => {}} />);
+
+    const goBackButton = screen.getByText('Go back');
+
+    fireEvent.press(goBackButton);
+
+    expect(mockGoBack).toHaveBeenCalled();
+  });
+
   //TODO : to improve the tests with mocking the auth
+
+  // it('should display error message when reset password email fails to send', async () => {
+  //   // jest.spyOn(auth(), 'sendPasswordResetEmail');
+  //   // Extend the type of the mock to accept an email address as an argument
+  //   (auth().sendPasswordResetEmail as jest.Mock).mockResolvedValue(undefined);
+  //
+  //   render(<ResetPassword navigation={() => {}} route={() => {}} />);
+  //
+  //   // const emailInput = screen.getByPlaceholderText('Email');
+  //   const sendResetPasswordButton = screen.getByText('Send →');
+  //
+  //   // fireEvent.changeText(emailInput, 'test@test.com');
+  //   fireEvent.press(sendResetPasswordButton);
+  //
+  //   expect(auth().sendPasswordResetEmail).toHaveBeenCalledWith('test@test.com');
+  //
+  //   // await waitFor(() => {
+  //   //   expect(auth().sendPasswordResetEmail).toHaveBeenCalledWith(
+  //   //     'test@test.com'
+  //   //   );
+  //   // });
+  // });
 
   // it('should send reset password email successfully when email is valid', async () => {
   //   const email = 'joe.bloggs@example.com';
